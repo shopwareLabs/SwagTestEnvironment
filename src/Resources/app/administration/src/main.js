e3487c4441f7962815ea28320d88d4658ddfbc8d
Soner Sayakci <s.sayakci@shopware.com>
2021-09-27 11:10:55 +0000

    feat: Initial commit

---
+++ src/Resources/app/administration/src/main.js
@@ -0,0 +1,11 @@
+const { Application } = Shopware;
+import CodeEditor from "./api/code-editor";
+
+Application.addServiceProvider('codeEditorApiService', (container) => {
+    const initContainer = Application.getContainer('init');
+
+    return new CodeEditor(initContainer.httpClient, container.loginService);
+});
+
+import './module/code-editor';
+import './module/mail-catcher';
